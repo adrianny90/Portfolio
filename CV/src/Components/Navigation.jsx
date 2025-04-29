077073498a3a8ae908f58c39bc1374e7040dc762
Adrian B <adrianny90@wp.pl>
2025-04-29 14:37:23 +0200

    small change

--- CV/src/Components/Navigation.jsx
+++ CV/src/Components/Navigation.jsx
@@ -1,51 +1,86 @@
-import { FaGithub, FaLinkedin } from "react-icons/fa";
-
+import ReactCountryFlag from "react-country-flag";
+import { useState } from "react";
 export default function Navigation() {
+  const [lang, setLang] = useState(false);
+  const callLand = () => {
+    setLang((prev) => !prev);
+  };
   return (
     <nav className="bg-gray-800 fixed w-full top-0 z-50 shadow-lg">
-      <div className="mx-auto px-6 sm:px-8">
-        <ul className="flex flex-wrap justify-end items-center py-2 gap-3 text-base sm:text-lg md:text-xl font-medium">
-          <li>
-            <a
-              href="#home"
-              className="hover:text-teal-400 transition-colors duration-300"
-            >
-              Home
-            </a>
-          </li>
-          <li>
-            <a
-              href="#skills"
-              className="hover:text-teal-400 transition-colors duration-300"
-            >
-              Skills
-            </a>
-          </li>
-          <li>
-            <a
-              href="#projects"
-              className="hover:text-teal-400 transition-colors duration-300"
-            >
-              Projects
-            </a>
-          </li>
-          <li>
-            <a
-              href="#about"
-              className="hover:text-teal-400 transition-colors duration-300"
-            >
-              About
-            </a>
-          </li>
-          <li>
-            <a
-              href="#contact"
-              className="hover:text-teal-400 transition-colors duration-300"
-            >
-              Contact
-            </a>
-          </li>
-        </ul>
+      <div className=" mx-auto px-6 sm:px-8">
+        <div className="flex justify-between">
+          <div>
+            {" "}
+            {!lang && (
+              <button onClick={callLand}>
+                <ReactCountryFlag
+                  countryCode="US"
+                  svg
+                  style={{
+                    width: "3em",
+                    height: "3em",
+                  }}
+                  title="US"
+                />
+              </button>
+            )}
+            {lang && (
+              <button onClick={callLand}>
+                <ReactCountryFlag
+                  countryCode="DE"
+                  svg
+                  style={{
+                    width: "3em",
+                    height: "3em",
+                  }}
+                  title="DE"
+                />
+              </button>
+            )}
+          </div>
+          <ul className="flex flex-wrap justify-end items-center py-2 gap-3 text-base sm:text-lg md:text-xl font-medium">
+            <li>
+              <a
+                href="#home"
+                className="hover:text-teal-400 transition-colors duration-300"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#skills"
+                className="hover:text-teal-400 transition-colors duration-300"
+              >
+                Skills
+              </a>
+            </li>
+            <li>
+              <a
+                href="#projects"
+                className="hover:text-teal-400 transition-colors duration-300"
+              >
+                Projects
+              </a>
+            </li>
+            <li>
+              <a
+                href="#about"
+                className="hover:text-teal-400 transition-colors duration-300"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="#contact"
+                className="hover:text-teal-400 transition-colors duration-300"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
