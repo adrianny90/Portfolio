e4fe21b36dc32a62f514a6329e79e4de1eec17ce
Adrian B <adrianny90@wp.pl>
2025-05-23 16:31:40 +0200

    small env change

--- CV/src/email/contactMe.js
+++ CV/src/email/contactMe.js
@@ -1,6 +1,6 @@
 export const contactMe = async (name, email, message) => {
   const formData = { name: name, email: email, message: message };
-  const res = await fetch(`http://localhost:3000/contact`, {
+  const res = await fetch(`${import.meta.env.BACKEND_URL}/contact`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(formData),
