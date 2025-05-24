e592f73f9e42efadd36eaca385008b3b5e799058
Adrian B <adrianny90@wp.pl>
2025-05-24 21:41:13 +0200

    small text change

--- CV/src/Components/ContactMe.jsx
+++ CV/src/Components/ContactMe.jsx
@@ -122,7 +122,7 @@ export default function ContactMe() {
         <section id="contact" className="py-16 bg-gray-900">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <h2 className="text-3xl md:text-4xl font-bold text-teal-400 text-center mb-12">
-              Kontaktiere mich
+              Kontaktieren Sie mich
             </h2>
             <motion.form
               className="max-w-lg mx-auto"
