b0f45d00a5672f24aa5017a65e06a97b4886f178
Adrian B <adrianny90@wp.pl>
2025-05-24 22:01:37 +0200

    message change

--- e_commerce_fake/src/components/Payment.jsx
+++ e_commerce_fake/src/components/Payment.jsx
@@ -1,11 +1,10 @@
-import { clearCart } from "../utils/helpers.js";
 const Payment = ({ shop, setShop }) => {
   const clearCart = () => {
     setShop([]);
   };
   const payNow = () => {
     alert(
-      "You have got it  !!! \nYour paymant is being proceed, please expect delivery soon!\nThank you for your trust."
+      "Success!\n\nYour payment is processing. Expect delivery soon.\n\nThank you!"
     );
     clearCart();
   };
